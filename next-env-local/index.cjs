9bbefb4f02bdff72a11ad3024c37b1f60feff78a
Scott Motte <mot@mot.la>
2026-06-19 10:08:21 -0700

    try to grab modules

--- next-env-local/index.cjs
+++ next-env-local/index.cjs
@@ -1,11 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-let dotenvx
-try {
-  dotenvx = require('/var/task/node_modules/@dotenvx/dotenvx')
-} catch (_) {
-  dotenvx = require('@dotenvx/dotenvx')
-}
+const dotenvx = require('./node_modules/@dotenvx/dotenvx')
 
 let initialEnv
 let combinedEnv
